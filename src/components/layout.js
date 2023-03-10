604aa6888ef8aa8a68c4590f6b4339365419a58a
Caleb Faulkner <caleb.faulkner@icloud.com>
2023-03-10 14:35:57 -0500

    animation / components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,41 +1,19 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
-
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import PropTypes from 'prop-types'
 
 import Header from "./header"
+import Footer from "./footer"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-    </>
+const Layout =({children}) =>{
+  return(
+      <>
+      <div>
+          <Header/>
+          <Footer/>
+      </div>
+      <main>{children}</main>
+      </>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,68 +7,44 @@ import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
 const IndexPage = () => (
-<body>
-  <div id="headerContainer">
-      <header>
-          <img id="profilePic" src="images/Finished-min.jpg"></img>
-          <section id="title">
-              <h1>MY NAME IS CALEB FAULKNER</h1>
-              <p class="bodyCopy">DESIGN TECHNOLOGIST - <a id="role" href="https://tactacam.com" target="blank">TACTACAM</a></p>
-          </section>
-          <section id="flagContainer">
-              <img class="flags" src="images/Flag-US.png"></img>
-              <img class="flags" src="images/Flag-Earth.png"></img>
-          </section>
-      </header>
-  </div>
-  <div id="mainContainer">
-      <main id="cardContainer">
-          <section>
-              <div class="mainCard">
-                  <img src="images/card1.png"></img>
-              </div>
-              <p class="s1">PROFESSIONAL PROJECTS</p>
-          </section>
-          <section>
-              <div class="mainCard">
-                  <img src="images/card2.png"></img>
-              </div>
-              <p class="s1">PERSONAL PROJECTS</p>
-          </section>
-          <a HREF = "stack.html">
-              <section>
-                  <div class="mainCard">
-                      <img src="images/card3.png"></img>
-                  </div>
-                  <p class="s1">STACK</p>
-              </section>
-          </a>
-          <section>
-              <div class="mainCard">
-                  <img src="images/card4.png"></img>
-              </div>
-              <p class="s1">CREDENTIALS</p>
-          </section>
-      </main>
-  </div>
-  <footer>
-      <div id="line"></div>
-      <div id="gradient"></div>
-      <div class="scroll">
-          <p class="scrollContent footerCopy">UX DESIGN - UI DESIGN - GRAPHIC DESIGN - MOTION GRAPHICS - 3D - FRONT END -</p>
-          <p class="scrollContent footerCopy">UX DESIGN - UI DESIGN - GRAPHIC DESIGN - MOTION GRAPHICS - 3D - FRONT END -</p>
-          <p class="scrollContent footerCopy">UX DESIGN - UI DESIGN - GRAPHIC DESIGN - MOTION GRAPHICS - 3D - FRONT END -</p>
-          <p class="scrollContent footerCopy">UX DESIGN - UI DESIGN - GRAPHIC DESIGN - MOTION GRAPHICS - 3D - FRONT END -</p>
-      </div>
-  </footer>
-</body>
+<>
+  <Layout>
+    <body>
+        <div id="mainContainer">
+            <main id="cardContainer">
+                <section id="section1">
+                    <div class="mainCard">
+                        <img src="images/card1.png"></img>
+                    </div>
+                    <p class="s1">PROFESSIONAL PROJECTS</p>
+                </section>
+                <section id="section2">
+                    <div class="mainCard">
+                        <img src="images/card2.png"></img>
+                    </div>
+                    <p class="s1">PERSONAL PROJECTS</p>
+                </section>
+                <a HREF = "stack.html">
+                    <section id="section3">
+                        <div class="mainCard">
+                            <img src="images/card3.png"></img>
+                        </div>
+                        <p class="s1">STACK</p>
+                    </section>
+                </a>
+                <section id="section4">
+                    <div class="mainCard">
+                        <img src="images/card4.png"></img>
+                    </div>
+                    <p class="s1">CREDENTIALS</p>
+                </section>
+            </main>
+        </div>
+    </body>
+  </Layout>
+</>
 )
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
