29190bacf72060d1f26e767ecb12be1f24b5a839
Caleb Faulkner <caleb.faulkner@icloud.com>
2023-03-15 21:12:16 -0400

    props and spacer fix

--- src/components/backHeader.js
+++ src/components/backHeader.js
@@ -3,15 +3,15 @@ import AniLink from "gatsby-plugin-transition-link/AniLink";
 
 import "./header.css";
 
-const BackButtonHeader = () => {
-    return (
+const BackButtonHeader = props => {
+  return (
     <div id="headerContainer">
       <header>
-        <AniLink fade to="/" duration={1}>
-            <img src="/images/arrow-left.svg" />
+        <AniLink id="backButton" fade to="/" duration={1}>
+            <img src="/images/arrow-left.svg" alt="Back" />
         </AniLink>
         <div id="titleContainer">
-          <h1>ABOUT</h1>
+          <h1>{props.title}</h1>
         </div>
         <section id="flagContainer">
           <img className="flags" src="/images/Flag-US.png" alt="US Flag" />
