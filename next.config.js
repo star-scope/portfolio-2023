119c8e9c0e81c9d662dff59748ba8309dd5c9fdf
star-scope <caleb.faulkner@icloud.com>
2023-08-04 23:05:45 -0400

    small commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-2023/' : '',
+  }
+  
